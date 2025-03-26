4cd567c117ce0d859ec85dcb8682a0cb3938f0f1
itorly <itorly@163.com>
2025-03-26 23:59:43 +0800

    basic

--- quickstart/basic.py
+++ quickstart/basic.py
@@ -1,3 +1,8 @@
 import numpy as np
 a = np.arange(15).reshape(3, 5)
-print(a)
+print('a = ', a)
+print('a.shape = ', a.shape)
+print('a.ndim = ', a.ndim)
+print('a.dtype = ', a.dtype.name)
+print('a.dtype.itemsize = ', a.itemsize)
+print('a.size = ', a.size)

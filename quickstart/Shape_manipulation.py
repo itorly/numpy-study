5ff23a5354121655cf2cce1f9e7b96ee19837988
itorly <itorly@163.com>
2025-03-30 17:54:48 +0800

    2.2 Stacking together different arrays: vstack, hstack, a[:, newaxis], slicing in 1D array

--- quickstart/Shape_manipulation.py
+++ quickstart/Shape_manipulation.py
@@ -35,3 +35,44 @@ print('a =', a)
 print('a.reshape(4, -1) =', a.reshape(4, -1))
 
 
+# 2.2 Stacking together different arrays
+print('\n### 2.2 Stacking together different arrays')
+
+# Several arrays can be stacked together along different axes
+# vstack, hstack
+a = np.floor(10 * rg.random((2, 2)))
+print('a =\n', a)
+b = np.floor(10 * rg.random((2, 2)))
+print('b =\n', b)
+# np.vstack((a, b))
+print('np.vstack((a, b)) =\n', np.vstack((a, b)))
+# np.hstack((a, b))
+print('np.hstack((a, b)) =\n', np.hstack((a, b)))
+
+# The function column_stack stacks 1D arrays as columns into a 2D array.
+# It is equivalent to hstack only for 2D arrays:
+from numpy import newaxis
+
+# with 2D arrays
+print('np.column_stack((a, b)) =\n', np.column_stack((a, b)))
+
+a = np.array([4., 2.])
+b = np.array([3., 8.])
+# returns a 2D array
+print('np.column_stack((a, b)) =\n', np.column_stack((a, b)))
+
+# 1D arrays
+print('np.hstack((a, b)) =\n', np.hstack((a, b)))
+
+# view `a` as a 2D column vector
+print('a[:, newaxis] =\n', a[:, newaxis])
+
+# np.column_stack((a[:, newaxis], b[:, newaxis]))
+print('np.column_stack((a[:, newaxis], b[:, newaxis])) =\n', np.column_stack((a[:, newaxis], b[:, newaxis])))
+
+# np.hstack((a[:, newaxis], b[:, newaxis]))  # the result is the same
+print('np.hstack((a[:, newaxis], b[:, newaxis])) =\n', np.hstack((a[:, newaxis], b[:, newaxis])))
+
+# In complex cases, r_ and c_ are useful for creating arrays by stacking numbers along one axis. They allow the use of range literals
+print('np.r_[0:4, 5, 6] =\n', np.r_[0:4, 5, 6])
+print('np.r_[1:4, 0, 4] =\n', np.r_[1:4, 0, 4])

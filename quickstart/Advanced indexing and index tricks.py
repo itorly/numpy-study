8cc70030b5d20f82bcbd2f13950596d7a84efc4e
itorly <itorly@163.com>
2025-03-31 23:07:22 +0800

    # 4.1.2 # the indexed array : a multidimensional array # the dimension of indexes : 1 # the shape of an index : a bidimensional array

--- quickstart/Advanced indexing and index tricks.py
+++ quickstart/Advanced indexing and index tricks.py
@@ -27,5 +27,26 @@ print('a[j] =\n', a[j])
 # IndexError: too many indices for array: array is 1-dimensional, but 2 were indexed
 # print('a[[3, 4], [9, 7]] =\n', a[[3, 4], [9, 7]])
 
+# 4.1.2
+# the indexed array : a multidimensional array
+# the dimension of indexes : 1
+# the shape of an index : a bidimensional array
+print('# 4.1.2')
+print('# the indexed array : a multidimensional array')
+print('# the dimension of indexes : 1')
+print('# the shape of an index : a bidimensional array')
+
+# When the indexed array a is multidimensional,
+# a single array of indices refers to the first dimension of a.
+palette = np.array([[0, 0, 0],         # black
+                    [255, 0, 0],       # red
+                    [0, 255, 0],       # green
+                    [0, 0, 255],       # blue
+                    [255, 255, 255]])  # white
+image = np.array([[0, 1, 2, 0],  # each value corresponds to a color in the palette
+                  [0, 3, 4, 0]])
+# the (2, 4, 3) color image
+print('palette[image] =\n', palette[image])
+
 
 

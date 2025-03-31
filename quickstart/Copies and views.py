935ba58d0774dc26a00b7d418263a2ee278de202
itorly <itorly@163.com>
2025-03-31 16:45:01 +0800

    3.2 View or shallow copy

--- quickstart/Copies and views.py
+++ quickstart/Copies and views.py
@@ -19,3 +19,34 @@ print('id(a) =', id(a))
 
 # may vary
 f(a)
+
+
+# 3.2 View or shallow copy
+print('\n# 3.2 View or shallow copy')
+
+c = a.view()
+# type
+print('type(c) =', type(c))
+# c is a
+print('c is a:', c is a)
+# c.base is a            # c is a view of the data owned by a
+print('c.base is a:', c.base is a)
+# c.flags.owndata
+print('c.flags.owndata:', c.flags.owndata)
+# c = c.reshape((2, 6))  # a's shape doesn't change, reassigned c is still a view of a
+c = c.reshape((2, 6))
+# a.shape
+print('a.shape:', a.shape)
+# c[0, 4] = 1234         # a's data changes
+print('a:\n', a)
+# a
+print('c:\n', c)
+
+# Slicing an array returns a view of it
+s = a[1:2, 1:3]
+print('s:\n', s)
+print('a:\n', a)
+s[:] = -1  # s[:] is a view of s. Note the difference between s = 10 and s[:] = 10
+print('s:\n', s)
+print('a:\n', a)
+

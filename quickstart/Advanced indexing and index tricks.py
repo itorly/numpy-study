04db774a5c39b3d84cbbc370d76ef1eced653385
itorly <itorly@163.com>
2025-04-02 18:47:32 +0800

    4.2.3 example: mandelbrot

--- quickstart/Advanced indexing and index tricks.py
+++ quickstart/Advanced indexing and index tricks.py
@@ -158,3 +158,33 @@ a
 #        [4, 0, 0, 0],
 #        [0, 0, 0, 0]])
 
+# 4.2.3 example: mandelbrot
+print('# 4.2.3 example: mandelbrot')
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def mandelbrot(h, w, maxit=20, r=2):
+    """Returns an image of the Mandelbrot fractal of size (h,w)."""
+    x = np.linspace(-2.5, 1.5, 4 * h + 1)
+    y = np.linspace(-1.5, 1.5, 3 * w + 1)
+    A, B = np.meshgrid(x, y)
+    C = A + B * 1j
+    z = np.zeros_like(C)
+    divtime = maxit + np.zeros(z.shape, dtype=int)
+
+    for i in range(maxit):
+        z = z ** 2 + C
+        diverge = abs(z) > r  # who is diverging
+        div_now = diverge & (divtime == maxit)  # who is diverging now
+        divtime[div_now] = i  # note when
+        z[diverge] = r  # avoid diverging too much
+
+    return divtime
+
+
+plt.clf()
+plt.imshow(mandelbrot(400, 400, 20, 2))
+plt.show()  # This line is crucial to display the plot
+# plt.savefig('mandelbrot.png')
+

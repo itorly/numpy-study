ea080df09fcdf178dbf5a4bf4f99ea6661b268b2
itorly <itorly@163.com>
2025-04-03 22:03:09 +0800

    5.3 Histograms

--- quickstart/Tricks and tips.py
+++ quickstart/Tricks and tips.py
@@ -24,3 +24,20 @@ print('m =\n', m)
 xy = np.hstack([x, y])
 # xy
 print('xy =\n', xy)
+
+# 5.3 Histograms
+print('\n# 5.3 Histograms')
+
+import numpy as np
+rg = np.random.default_rng(1)
+import matplotlib.pyplot as plt
+# Build a vector of 10000 normal deviates with variance 0.5^2 and mean 2
+mu, sigma = 2, 0.5
+v = rg.normal(mu, sigma, 10000)
+# Plot a normalized histogram with 50 bins
+plt.hist(v, bins=50, density=True)       # matplotlib version (plot)
+# Compute the histogram with numpy and then plot it
+(n, bins) = np.histogram(v, bins=50, density=True)  # NumPy version (no plot)
+plt.plot(.5 * (bins[1:] + bins[:-1]), n)
+plt.show()
+plt.savefig('histogram')
